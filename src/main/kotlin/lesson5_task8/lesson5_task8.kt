4a77af98f806d8508b9138d78d0e4de343f07e95
helikonia <helikonia99@gmail.com>
2025-06-17 22:15:00 +0200

    KS-5-5-8 formatted

--- src/main/kotlin/lesson5_task8/lesson5_task8.kt
+++ src/main/kotlin/lesson5_task8/lesson5_task8.kt
@@ -6,7 +6,9 @@ import org.jsoup.select.Elements
 
 fun main() {
 
-    val doc: Document = Jsoup.connect("https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations").get()
+    val doc: Document =
+        Jsoup.connect("https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations")
+            .get()
     val element: Elements = doc.select("div.sc-2aegk7-2")
     val quotes = element.text()
     println(quotes)
